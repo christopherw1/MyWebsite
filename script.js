8fca3a8402b4a8ac599eb24fafb7fd9ccf42175f
christopherw1 <ctwang3@illinois.edu>
2025-06-16 16:44:16 -0500

    initial gpt

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Console log when page loads
+console.log("Website loaded!");
+
+// Example: Alert when clicking a link (just for demo)
+document.querySelectorAll('.links a').forEach(link => {
+    link.addEventListener('click', () => {
+        console.log(`Going to: ${link.href}`);
+    });
+});
